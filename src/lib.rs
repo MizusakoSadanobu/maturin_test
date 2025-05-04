dd28b26d2f8ba1a41e5c73ec4bd88485345c150a
sadanobu.mizusako <sadanobu.mizusako@gmail.com>
2025-05-04 10:42:55 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,16 @@ use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn sum_as_string(a: &[usize]) -> PyResult<String> {
+    let sum = 0;
+    for v in a {
+        sum += v;
+    }
+    Ok(sum.to_string())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-#[pyo3(name="_lib_name")]
 fn maturin_test(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
