71bc237e84ae58a56ab364257fba158653eb9a2e
sadanobu.mizusako <sadanobu.mizusako@gmail.com>
2025-05-04 10:56:22 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
 use pyo3::prelude::*;
+use numpy::{PyReadonlyArray1, IntoPyArray};
 
-/// Formats the sum of two numbers as string.
+/// PythonのNumPy配列の合計を文字列で返す
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    // let sum = 0;
-    // for v in a {
-    //     sum += v;
-    // }
-    Ok((a+b).to_string())
+fn sum_as_string(array: PyReadonlyArray1<f64>) -> PyResult<String> {
+    let sum = array.as_array().sum();
+    Ok(sum.to_string())
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn maturin_test(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
